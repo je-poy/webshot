9da662bc18f31a8276d7eae89f61dcc4cca3c828
Jeff <Jeff>
2017-05-08 13:50:56 +0800

    Initial commit.

---
+++ navigator.js
@@ -0,0 +1,50 @@
+navigator.mediaDevices.getUserMedia = navigator.getUserMedia ||
+                         navigator.webkitGetUserMedia ||
+                         navigator.mozGetUserMedia;
+
+var constraints = { audio: false, video: true };
+
+var start   = document.querySelector('#start');
+var snapshot = document.querySelector('#snapshot');
+var filter  = document.querySelector('#filter');
+var video   = document.querySelector('video');
+var canvas  = document.querySelector('canvas');
+
+var filters = ['blur', 'brightness', 'contrast', 'grayscale',
+               'hue', 'invert', 'saturate', 'sepia'];
+
+start.addEventListener('click', function() {
+    navigator.getUserMedia(constraints, success, error);
+});
+
+function success(stream) {
+   /* hide the start button*/
+   start.style.display = 'none';
+   
+   /* show the snapshot button*/
+   snapshot.style.display = 'block';
+   
+   /* show the filter button*/
+   filter.style.display = 'block';
+   if(window.URL) {
+      video.src = window.URL.createObjectURL(stream);
+   } else {
+      video.src = stream;
+   }
+}
+
+function error(e) {
+   console.log('navigator.getUserMedia error: ', e);
+}
+
+filter.addEventListener('click', function() {	
+   var index = (filters.indexOf(canvas.className) + 1) % filters.length;
+   video.className = filters[index];
+   canvas.className = filters[index];		
+});
+
+snapshot.addEventListener('click', function() {
+   canvas.width = 360;
+   canvas.height = 270;
+   canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+});
